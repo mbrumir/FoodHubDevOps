b0d11e36d709b468a1fa4f0c98202e9e2d206892
Jakub Świątkowski <jakub.swiatkowski@droptica.com>
2024-02-28 18:58:47 +0100

    Map clustering

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,15 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {
 	// AdvancedMarker,
 	APIProvider,
 	Map,
-	Marker
+	useMap,
+	AdvancedMarker
 	// Pin
 } from '@vis.gl/react-google-maps';
+
+import {MarkerClusterer} from '@googlemaps/markerclusterer';
+import type {Marker} from '@googlemaps/markerclusterer';
 import MapFilters from "./MapFilters";
 import MapObjectDetails from "./MapObjectDetails";
 import { db } from "../../firebase";
-import { collection, getDocs, where, query } from "@firebase/firestore";
+import { collection, getDocs, where, query, doc, getDoc, DocumentSnapshot } from "@firebase/firestore";
 
 const API_KEY = "AIzaSyButo3F2cEMH6mNiMGIhbqypnxY3YeMGq0";
 function MapComponent() {
@@ -18,23 +22,54 @@ function MapComponent() {
 	const [restaurantDetails, setRestaurantDetails] = useState<any>(false);
 	const [filters, setFilters] = useState<any>({}); // { creatorOption, foodOption, priceOption }
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				let restaurantsCollection = collection(db, "restaurants");
-				let q = query(
+	const fetchData = async () => {
+		try {
+			let restaurantsCollection = collection(db, "restaurants");
+			let q = query(
 					restaurantsCollection,
 					where("place", "!=", null)
-					);
+			);
+			const typesSnapshot = await getDocs(q);
+			let restaurantsData = typesSnapshot.docs.map((doc) => doc.data());
+			setRestaurants(restaurantsData);
+			localStorage.setItem('restaurants', JSON.stringify(restaurantsData));
+		} catch (error) {
+			console.error("Error fetching data: ", error);
+		}
+	};
 
-				const typesSnapshot = await getDocs(q);
-				setRestaurants(typesSnapshot.docs.map((doc) => doc.data()));
+	useEffect(() => {
+		const fetchConfigData = async () => {
+			try {
+				const videoConfigDocRef = doc(db, "config", "videos");
+				const videoConfigSnapshot: DocumentSnapshot<any> = await getDoc(videoConfigDocRef);
+				let updated = videoConfigSnapshot.data().updated;
+				let localUpdated = localStorage.getItem('updated');
+				if (!localUpdated) {
+					updated = new Date().getTime();
+				}
+
+				if (localUpdated != updated) {
+					localStorage.setItem('updated', updated);
+					fetchData();
+				}
+
 			} catch (error) {
 				console.error("Error fetching data: ", error);
 			}
-		};
 
-		fetchData();
+		}
+
+		fetchConfigData();
+	}, []);
+
+	useEffect(() => {
+		let localRestaurants = JSON.parse(localStorage.getItem('restaurants') as string);
+		if (localRestaurants) {
+			setRestaurants(localRestaurants);
+		} else {
+			fetchData();
+		}
 	}, [filters]);
 
 	useEffect(() => {
@@ -48,14 +83,73 @@ function MapComponent() {
 		setMarkers(markers);
 	}, [restaurants]);
 
-	
+
 	function hideFooter() {
 		const footer = document.querySelector('footer') as HTMLElement;
 		const restaurantDetails = document.querySelector('div.map--restaurant-details') as HTMLElement;
-		
+
 		if (restaurantDetails && restaurantDetails.style.display === 'flex') {
 			footer.classList.add('hide');
 		}
+	}
+
+	type Props = {points: any[]};
+
+	const Markers = ({points} : Props) => {
+		const map = useMap();
+		const [markers, setMarkers] = useState<{[key: string]: Marker}>({});
+		const clusterer = useRef<MarkerClusterer | null>(null);
+
+		// Initialize MarkerClusterer
+		useEffect(() => {
+			console.log(123);
+			if (!map) return;
+			if (!clusterer.current) {
+				clusterer.current = new MarkerClusterer({map});
+			}
+		}, [map]);
+
+		// Update markers
+		useEffect(() => {
+			clusterer.current?.clearMarkers();
+			clusterer.current?.addMarkers(Object.values(markers));
+		}, [markers]);
+
+		const setMarkerRef = (marker: Marker | null, key: string) => {
+			if (marker && markers[key]) return;
+			if (!marker && !markers[key]) return;
+
+			setMarkers(prev => {
+				if (marker) {
+					return {...prev, [key]: marker};
+				} else {
+					const newMarkers = {...prev};
+					delete newMarkers[key];
+					return newMarkers;
+				}
+			});
+		};
+
+		return (
+				<>
+					{points.map(point => (
+							<AdvancedMarker
+									position={point}
+									key={point.key}
+									ref={marker => setMarkerRef(marker, point.key)}
+									onClick={() => {
+										handleShowRestaurantDetails();
+										setRestaurantDetails(point.name);
+										setTimeout(() => {
+											hideFooter();
+										}, 1);
+									}}
+							>
+								<span className="tree">🌳</span>
+							</AdvancedMarker>
+					))}
+				</>
+		);
 	}
 
 	function handleShowRestaurantDetails() {
@@ -77,21 +171,22 @@ function MapComponent() {
 					disableDefaultUI>
 				<MapFilters setFilters={setFilters}/>
 				<MapObjectDetails restaurantName={restaurantDetails}/>
-				{ markers.map((marker, index) => (
-					<Marker
-						position={marker}
-						clickable={true}
-						onClick={() => {
-							handleShowRestaurantDetails();
-							setRestaurantDetails(marker.name);
-							setTimeout(() => {
-								hideFooter();
-							}, 1);
-						}}
-						title={marker.name}
-					/>
-				))}
-				{/* simple marker */}
+				<Markers points={markers} />
+				{/*{ markers.map((marker, index) => (*/}
+				{/*	<Marker*/}
+				{/*		position={marker}*/}
+				{/*		clickable={true}*/}
+				{/*		onClick={() => {*/}
+				{/*			handleShowRestaurantDetails();*/}
+				{/*			setRestaurantDetails(marker.name);*/}
+				{/*			setTimeout(() => {*/}
+				{/*				hideFooter();*/}
+				{/*			}, 1);*/}
+				{/*		}}*/}
+				{/*		title={marker.name}*/}
+				{/*	/>*/}
+				{/*))}*/}
+				{/*/!* simple marker *!/*/}
 			</Map>
 		</APIProvider>
 	);
